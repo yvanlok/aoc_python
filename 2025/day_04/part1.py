7b5acd79803f1aa1da67564d01ff33b88cad3231
Yvan Lokhmotov <yvan.lokhmotov@icloud.com>
2025-12-04 19:52:32 +0000

    Implement solutions for Advent of Code 2025 - Day 4, Parts 1 and 2

--- 2025/day_04/part1.py
+++ 2025/day_04/part1.py
@@ -11,12 +11,33 @@ def read_input(filename="data.txt"):
         return f.read().strip()
 
 
+def adjacent_rolls(grid, y, x):
+
+    count = 0
+
+    for j in range(y - 1, y + 2):
+        for i in range(x - 1, x + 2):
+            if j == y and i == x:
+                continue
+            if 0 <= j < len(grid) and 0 <= i < len(grid[0]):
+                if grid[j][i] == "@":
+                    count += 1
+    return count
+
+
 def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
+    grid = [list(line) for line in lines]
 
-    # TODO: Implement solution
-    result = None
+    result = 0
+
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == "@":
+                rolls = adjacent_rolls(grid, y, x)
+                if rolls < 4:
+                    result += 1
 
     return result
 
--- 2025/day_04/part2.py
+++ 2025/day_04/part2.py
@@ -1,5 +1,5 @@
 """
-Advent of Code 2025 - Day 4 - Part 2
+Advent of Code 2025 - Day 4 - Part 1
 """
 
 from time import perf_counter
@@ -11,12 +11,43 @@ def read_input(filename="data.txt"):
         return f.read().strip()
 
 
+def adjacent_rolls(grid, y, x):
+
+    count = 0
+
+    for j in range(y - 1, y + 2):
+        for i in range(x - 1, x + 2):
+            if j == y and i == x:
+                continue
+            if 0 <= j < len(grid) and 0 <= i < len(grid[0]):
+                if grid[j][i] == "@":
+                    count += 1
+    return count
+
+
 def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
+    grid = [list(line) for line in lines]
 
-    # TODO: Implement solution
-    result = None
+    result = 0
+
+    finished = False
+
+    while not finished:
+        to_remove = []
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == "@":
+                    rolls = adjacent_rolls(grid, y, x)
+                    if rolls < 4:
+                        result += 1
+                        to_remove.append((y, x))
+        if to_remove:
+            for y, x in to_remove:
+                grid[y][x] = "X"
+        else:
+            finished = True
 
     return result
 
