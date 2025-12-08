f00c203c772c617ebdb73655d9432469e8a0b1cb
Yvan Lokhmotov <yvan.lokhmotov@icloud.com>
2025-12-08 18:44:16 +0000

    Implement solution for Advent of Code 2025 - Day 8, Parts 1 and 2

--- 2025/day_08/part1.py
+++ 2025/day_08/part1.py
@@ -3,6 +3,7 @@ Advent of Code 2025 - Day 8 - Part 1
 """
 
 from time import perf_counter
+import math
 
 
 def read_input(filename="data.txt"):
@@ -15,8 +16,42 @@ def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
 
-    # TODO: Implement solution
-    result = None
+    positions = [[int(x) for x in line.split(",")] for line in lines]
+
+    junctions = 1000
+
+    circuits = [[i] for i in range(0, len(positions))]
+
+    distances = {}
+
+    for i in range(0, len(positions)):
+        for j in range(i + 1, len(positions)):
+            distances[(i, j)] = math.dist(positions[i], positions[j])
+
+    sorted_distances = sorted(distances.items(), key=lambda x: x[1])
+
+    for i in range(0, junctions):
+        (box_1, box_2), distance = sorted_distances[i]
+
+        # Find the circuits that box_1 and box_2 belong to
+        circuit_1 = next(circuit for circuit in circuits if box_1 in circuit)
+        circuit_2 = next(circuit for circuit in circuits if box_2 in circuit)
+
+        # If they are in different circuits, merge them
+        if circuit_1 != circuit_2:
+            circuits.remove(circuit_1)
+            circuits.remove(circuit_2)
+            circuits.append(circuit_1 + circuit_2)
+
+    # Get the sizes of the circuits and sort them in descending order
+    circuit_sizes = sorted([len(circuit) for circuit in circuits], reverse=True)
+
+    # Multiply together the sizes of the 3 largest circuits
+    result = (
+        circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2]
+        if len(circuit_sizes) >= 3
+        else 0
+    )
 
     return result
 
--- 2025/day_08/part2.py
+++ 2025/day_08/part2.py
@@ -3,6 +3,7 @@ Advent of Code 2025 - Day 8 - Part 2
 """
 
 from time import perf_counter
+import math
 
 
 def read_input(filename="data.txt"):
@@ -15,8 +16,44 @@ def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
 
-    # TODO: Implement solution
-    result = None
+    positions = [[int(x) for x in line.split(",")] for line in lines]
+
+    circuits = [[i] for i in range(0, len(positions))]
+
+    distances = {}
+
+    for i in range(0, len(positions)):
+        for j in range(i + 1, len(positions)):
+            distances[(i, j)] = math.dist(positions[i], positions[j])
+
+    sorted_distances = sorted(distances.items(), key=lambda x: x[1])
+
+    i = 0
+
+    final_connection = None
+
+    while len(circuits) > 1:
+        (box_1, box_2), distance = sorted_distances[i]
+
+        # Find the circuits that box_1 and box_2 belong to
+        circuit_1 = next(circuit for circuit in circuits if box_1 in circuit)
+        circuit_2 = next(circuit for circuit in circuits if box_2 in circuit)
+
+        # If they are in different circuits, merge them
+        if circuit_1 != circuit_2:
+            circuits.remove(circuit_1)
+            circuits.remove(circuit_2)
+            circuits.append(circuit_1 + circuit_2)
+
+            # Check if this was the final merge
+            if len(circuits) == 1:
+                final_connection = (box_1, box_2)
+                break
+
+        i += 1
+
+    # Calculate result based on the final connection
+    result = positions[final_connection[0]][0] * positions[final_connection[1]][0]
 
     return result
 
