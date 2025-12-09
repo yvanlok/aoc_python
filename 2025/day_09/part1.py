c029078264146f12cb15d48e539339b767b09bcc
Yvan Lokhmotov <yvan.lokhmotov@icloud.com>
2025-12-09 17:37:34 +0000

    Implement solutions for Advent of Code 2025 - Day 9, Parts 1 and 2

--- 2025/day_09/part1.py
+++ 2025/day_09/part1.py
@@ -15,8 +15,18 @@ def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
 
-    # TODO: Implement solution
-    result = None
+    tile_locations = [tuple(map(int, tile.split(","))) for tile in lines]
+
+    max_area = 0
+
+    for i in range(len(tile_locations)):
+        for j in range(i, len(tile_locations)):
+            x_distance = abs(tile_locations[i][0] - tile_locations[j][0]) + 1
+            y_distance = abs(tile_locations[i][1] - tile_locations[j][1]) + 1
+
+            max_area = max(max_area, x_distance * y_distance)
+
+    result = max_area
 
     return result
 
--- 2025/day_09/part2.py
+++ 2025/day_09/part2.py
@@ -3,6 +3,7 @@ Advent of Code 2025 - Day 9 - Part 2
 """
 
 from time import perf_counter
+from shapely.geometry import Polygon
 
 
 def read_input(filename="data.txt"):
@@ -14,11 +15,37 @@ def read_input(filename="data.txt"):
 def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
+    red_tiles = [tuple(map(int, tile.split(","))) for tile in lines]
 
-    # TODO: Implement solution
-    result = None
+    # Create polygon from red tiles
+    polygon = Polygon(red_tiles)
 
-    return result
+    max_area = 0
+
+    # Check all pairs of red tiles
+    for i, (col_i, row_i) in enumerate(red_tiles):
+        for col_j, row_j in red_tiles[i + 1 :]:
+            # Create rectangle polygon
+            min_col = min(col_i, col_j)
+            max_col = max(col_i, col_j)
+            min_row = min(row_i, row_j)
+            max_row = max(row_i, row_j)
+
+            rectangle = Polygon(
+                [
+                    (min_col, min_row),
+                    (max_col, min_row),
+                    (max_col, max_row),
+                    (min_col, max_row),
+                ]
+            )
+
+            # Check if rectangle is fully contained in the polygon
+            if polygon.contains(rectangle):
+                area = (max_col - min_col + 1) * (max_row - min_row + 1)
+                max_area = max(max_area, area)
+
+    return max_area
 
 
 def main():
