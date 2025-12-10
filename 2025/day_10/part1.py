3f350c30059562eeecf863e97a86e1ed468efb35
Yvan Lokhmotov <yvan.lokhmotov@icloud.com>
2025-12-10 18:59:51 +0000

    Solve Day 10.
    Part 1 using BFS.
    Part 2 using linear programming and scipy.

--- 2025/day_10/part1.py
+++ 2025/day_10/part1.py
@@ -3,6 +3,7 @@ Advent of Code 2025 - Day 10 - Part 1
 """
 
 from time import perf_counter
+from collections import deque
 
 
 def read_input(filename="data.txt"):
@@ -15,10 +16,64 @@ def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
 
-    # TODO: Implement solution
-    result = None
+    diagrams = []
+    button_masks = []
+    # joltages = [] # probably in part 2
+
+    result = 0
+
+    for line in lines:
+
+        parts = line.split()
+        diagram = parts[0][1:-1]
+        wiring = parts[1:-1]
+
+        diagrams.append(diagram)
+
+        line_button_masks = []
+
+        for button in wiring:
+
+            button = button[1:-1].split(",")
+            button_mask = list("." * len(diagram))
+
+            for i in button:
+                button_mask[int(i)] = "#"
+
+            line_button_masks.append("".join(button_mask))
+
+        button_masks.append(line_button_masks)
+
+    for idx in range(0, len(diagrams)):
+        queue = deque()
+        queue.append(
+            ("." * len(diagrams[idx]), 0)
+        )  # (empty diagram, button presses) = 0
+        visited = set()
+        visited.add("." * len(diagrams[0]))  # don't repeat
+
+        while queue:
+            current = queue.popleft()
+            (mask, number) = current
+
+            # check if diagram has been reached
+            if mask == diagrams[idx]:
+                result += number
+                break
+            else:
+                # add each possible next mask to queue
+                for next_mask in button_masks[idx]:
+                    resultant_mask = xor_strings(mask, next_mask)
+                    # only add if not visited so far
+                    if resultant_mask not in visited:
+                        visited.add(resultant_mask)
+                        queue.append((resultant_mask, number + 1))
 
     return result
+
+
+def xor_strings(s1, s2):
+    return "".join("#" if c1 != c2 else "." for c1, c2 in zip(s1, s2))
 
 
 def main():
--- 2025/day_10/part2.py
+++ 2025/day_10/part2.py
@@ -3,6 +3,8 @@ Advent of Code 2025 - Day 10 - Part 2
 """
 
 from time import perf_counter
+import numpy as np
+from scipy.optimize import milp, LinearConstraint, Bounds
 
 
 def read_input(filename="data.txt"):
@@ -15,10 +17,58 @@ def solve(data):
     """Solve the puzzle."""
     lines = data.split("\n")
 
-    # TODO: Implement solution
-    result = None
+    result = 0
+
+    for line in lines:
+
+        parts = line.split()
+        wiring_strs = parts[1:-1]
+        joltage_str = parts[-1]
+
+        wiring = []
+
+        # Parse wiring as list of lists of integers
+        for button in wiring_strs:
+            button_indices = [int(x) for x in button[1:-1].split(",")]
+            wiring.append(button_indices)
+
+        # Parse joltages
+        target = [int(x) for x in joltage_str[1:-1].split(",")]
+
+        result += solve_lp(wiring, target)
 
     return result
+
+
+def solve_lp(wiring, target):
+    num_buttons = len(wiring)
+    num_counters = len(target)
+
+    # np matrix with column for each button, and row showing which counter it affects
+    A = np.zeros((num_counters, num_buttons))
+
+    for btn_idx, affected_counters in enumerate(wiring):
+        for counter_idx in affected_counters:
+            A[counter_idx, btn_idx] = 1
+
+    # each button costs 1 - want to minimise
+    c = np.ones(num_buttons)
+
+    # sum of counters must be same as joltages
+    constraints = LinearConstraint(A, target, target)
+
+    # result must be positive
+    bounds = Bounds(lb=0, ub=np.inf)
+
+    # solve using scipy.milp
+    result = milp(
+        c, constraints=constraints, bounds=bounds, integrality=np.ones(num_buttons)
+    )
+
+    if result.success:
+        return int(round(result.fun))
+    else:
+        return None
 
 
 def main():
